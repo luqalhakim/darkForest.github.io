9064be72df89628fa927690cef14f479f58d44d5
luqmanalhakim <ytmlhg@gmail.com>
2024-01-22 15:30:41 +0700

    add Footer and Scroll Button to Top

--- script.js
+++ script.js
@@ -119,6 +119,25 @@ var AutoTyping = (function (e) {
 // });
 // End Code of Scroll Down Button
 
-// Code for Hide Link Preview on left/right
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
 
-// End of Code of Hide Link Preview
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
